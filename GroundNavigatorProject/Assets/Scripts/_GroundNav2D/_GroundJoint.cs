bd768dc2d581be0d1377a88e9219f8788c45c9a6
Hanabi <nlime3141592@naver.com>
2022-07-30 01:39:35 +0900

    Add _GroundLedge.cs

--- GroundNavigatorProject/Assets/Scripts/_GroundNav2D/_GroundJoint.cs
+++ GroundNavigatorProject/Assets/Scripts/_GroundNav2D/_GroundJoint.cs
@@ -8,23 +8,37 @@ namespace JlMetroidvaniaProject.MapManagement
     [RequireComponent(typeof(CircleCollider2D))]
     public class _GroundJoint : _Ground
     {
-        public _GroundNavigator m_navigator;
-        public CircleCollider2D m_circleCollider;
-        public BoxCollider2D m_boxCollider;
-
+        [Header("Joint Collider Settings")]
+        public bool jointEnable = true;
         public JointShape jointShape = JointShape.Circle;
+        public bool ledgeGrabAvailable = false;
+        public bool syncBySlopeAngle = true;
+        public Vector2 jointPosition;
 
-        public Vector2 position;
+        [Header("Joint Gizmo Settings")]
+        public bool gizmoEnable = true;
+        public Color gizmoColor = Color.cyan;
+        public float gizmoSize = 1.0f;
+
+        private _GroundNavigator m_navigator;
+        private CircleCollider2D m_circleCollider;
+        private BoxCollider2D m_boxCollider;
+        public Vector2 boxJointSize => m_boxCollider.size;
 
         public enum JointShape
         {
             Circle, Box
         }
 
+        public enum LedgePivot
+        {
+            LeftTop, Top, RightTop, Left, Center, Right, LeftBottom, Bottom, RightBottom
+        }
+
         private void OnValidate()
         {
             m_navigator = GetComponentInParent<_GroundNavigator>();
-            transform.position = position;
+            transform.position = jointPosition;
             m_navigator._Validate();
         }
 
@@ -43,11 +57,15 @@ namespace JlMetroidvaniaProject.MapManagement
 
             if(index >= properties.m_count)
             {
+                transform.eulerAngles = Vector3.zero;
                 gameObject.SetActive(false);
             }
             else
             {
-                transform.eulerAngles = Vector3.forward * properties.m_eulerAngles[index];
+                if(syncBySlopeAngle)
+                    transform.eulerAngles = Vector3.forward * properties.m_eulerAngles[index];
+                else
+                    transform.eulerAngles = Vector3.zero;
                 transform.localScale = Vector3.one;
 
                 m_boxCollider.size = properties.m_boxSizes[index];
@@ -59,12 +77,12 @@ namespace JlMetroidvaniaProject.MapManagement
 
         private void OnDrawGizmos()
         {
-            Gizmos.color = Color.cyan;
+            Gizmos.color = gizmoColor;
 
             if(jointShape == JointShape.Circle)
-                Gizmos.DrawWireSphere(new Vector3(transform.position.x, transform.position.y, 0.0f), 0.5f);
+                Gizmos.DrawWireSphere(new Vector3(transform.position.x, transform.position.y, 0.0f), gizmoSize * 0.5f);
             else if(jointShape == JointShape.Box)
-                Gizmos.DrawWireCube(new Vector3(transform.position.x, transform.position.y, 0), new Vector3(1, 1, 0));
+                Gizmos.DrawWireCube(new Vector3(transform.position.x, transform.position.y, 0), new Vector3(gizmoSize, gizmoSize, 0));
         }
     }
 }
